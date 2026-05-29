f59b2d27a8d6b7f455d4efed0fe816b9510c2404
SEKEN123 <lpkdf123@gmail.com>
2026-05-29 23:21:16 +0800

    Menambahkan kode di file Player

--- app/src/main/java/squadsync/model/Player.java
+++ app/src/main/java/squadsync/model/Player.java
@@ -1,5 +1,23 @@
 package squadsync.model;
 
-public class Player {
-    
+public class Player extends Person {
+    private String position;
+    private int number;
+    private int score;
+
+    public Player(int id, String name, int age, String position, int number, int score) {
+        super(id, name, age);
+        this.position = position;
+        this.number = number;
+        this.score = score;
+    }
+
+    public String getPosition() { return position; }
+    public void setPosition(String position) { this.position = position; }
+
+    public int getNumber() { return number; }
+    public void setNumber(int number) { this.number = number; }
+
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
 }
