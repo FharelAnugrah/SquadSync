4e45047c363b223b3dc04b50b89b10843be8957d
SEKEN123 <lpkdf123@gmail.com>
2026-05-29 23:23:14 +0800

    Menambahkan kode di file Team

--- app/src/main/java/squadsync/model/Team.java
+++ app/src/main/java/squadsync/model/Team.java
@@ -1,3 +1,19 @@
+package squadsync.model;
+
+import java.util.List;
+
 public class Team {
-    
+    private List<Player> players;
+    private Coach coach;
+
+    public Team(List<Player> players, Coach coach) {
+        this.players = players;
+        this.coach = coach;
+    }
+
+    public List<Player> getPlayers() { return players; }
+    public void setPlayers(List<Player> players) { this.players = players; }
+
+    public Coach getCoach() { return coach; }
+    public void setCoach(Coach coach) { this.coach = coach; }
 }
