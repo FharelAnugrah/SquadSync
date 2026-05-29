53c3d94e574d50bfe58f8cc0c71937d4fc8b89ac
SEKEN123 <lpkdf123@gmail.com>
2026-05-29 23:19:02 +0800

    Menambahkan kode di file Coach

--- app/src/main/java/squadsync/model/Coach.java
+++ app/src/main/java/squadsync/model/Coach.java
@@ -1,5 +1,13 @@
 package squadsync.model;
 
-public class Coach {
-    
+public class Coach extends Person {
+    private String specialty;
+
+    public Coach(int id, String name, int age, String specialty) {
+        super(id, name, age);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() { return specialty; }
+    public void setSpecialty(String specialty) { this.specialty = specialty; }
 }
