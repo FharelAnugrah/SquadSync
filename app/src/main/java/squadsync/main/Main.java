f001b86aad00d771d1915b97a5b5391fc628a2ad
fharel <parelanugrah62@gmail.com>
2026-05-30 23:25:51 +0800

    Main

--- app/src/main/java/squadsync/main/Main.java
+++ app/src/main/java/squadsync/main/Main.java
@@ -1,3 +1,23 @@
-public class Main {
-    
+package squadsync.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import squadsync.database.DatabaseManager;
+import squadsync.ui.LoginScreen;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Initialize Database
+        DatabaseManager.getInstance();
+
+        // Start with Login Screen
+        LoginScreen loginScreen = new LoginScreen(primaryStage);
+        loginScreen.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
